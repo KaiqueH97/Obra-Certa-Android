0cea34c34324b2523bc229bf7751cb3a88c1d3ff
Kaique Santos <kaiqueh.felix@gmail.com>
2026-04-11 12:03:16 -0300

    feat: navegação do login e configuração do tema claro
    - Adicionada lógica em Kotlin para navegar do Login para o Home
    - Correção de duplicidade na porta de entrada no AndroidManifest
    - Configuração do aplicativo para forçar o Light Mode (tema claro)

--- app/src/main/java/com/example/obra_certa_android/LoginActivity.kt
+++ app/src/main/java/com/example/obra_certa_android/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.obra_certa_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+        btnEntrar.setOnClickListener {
+            val intentNavegacao = Intent(this, HomeActivity::class.java)
+            startActivity(intentNavegacao)
+            finish()
         }
     }
 }
